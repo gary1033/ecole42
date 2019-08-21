e8d426c85a0f0fbf5f40b1b1a6e26c0623d7b854
Rui-lin Chang <ruchang@e1z2r5p11.42.us.org>
2019-08-21 10:06:34 -0700

    add

--- day11/ex15/ft_list_reverse_fun.c
+++ day11/ex15/ft_list_reverse_fun.c
@@ -6,10 +6,11 @@
 /*   By: ruchang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/20 14:53:54 by ruchang           #+#    #+#             */
-/*   Updated: 2019/08/20 14:57:19 by ruchang          ###   ########.fr       */
+/*   Updated: 2019/08/21 10:06:01 by ruchang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include "ft_list.h"
 
@@ -17,16 +18,21 @@ void	ft_list_reverse_fun(t_list *begin_list)
 {
 	t_list	*tmp;
 	t_list	*head;
+	t_list	*store;
 
-	head = begin_list;
-	head->next = NULL;
-	begin_list = begin_list->next;
+	tmp = begin_list;
+	store = NULL;
+	while (tmp)
+	{
+		store = head;
+		head = ft_create_elem(tmp->data);
+		head->next = store;
+		tmp = tmp->next;
+	}
 	while (begin_list)
 	{
-		tmp = begin_list;
+		begin_list->data = head->data;
 		begin_list = begin_list->next;
-		tmp->next = head;
-		head = tmp;
+		head = head->next;
 	}
-	begin_list = head;
 }
